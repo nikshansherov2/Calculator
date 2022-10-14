18e452449197919d71774d85601a4b7a4bfa7205
Anton Vashenko <vashchenko-96@mail.ru>
2022-10-14 16:16:01 +0300

    Add in rational function sqrt ability

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ import logger as l
 
 
 def for_rat(choice):
-    num_1 = i.r_value_1()
-    num_2 = i.r_value_2()
-    if e.check_num(num_1) and e.check_num(num_2):
-        if choice in ['4', '5', '6'] and num_2 == '0':
-            print("На 0 делить нельзя")
-        else:
-            num_1, num_2 = eval(num_1), eval(num_2)
-            r.init(num_1, num_2)
+    if choice == "8":
+        num = i.sqrt_value()
+        if e.check_num(num):
+            r.init(eval(num), 0)
             res = r.do_it(choice)
-            print(f"Результат: {res}\n")
-            l.log(num_1, num_2, res, choice)
+            print(f"Результат: {res}")
+            l.log(num, 0, res, choice)
+        else:
+            print("Некорректный ввод")
     else:
-        print("Некорректный ввод")
+        num_1 = i.r_value_1()
+        num_2 = i.r_value_2()
+        if e.check_num(num_1) and e.check_num(num_2):
+            if choice in ['4', '5', '6'] and num_2 == '0':
+                print("На 0 делить нельзя")
+            else:
+                num_1, num_2 = eval(num_1), eval(num_2)
+                r.init(num_1, num_2)
+                res = r.do_it(choice)
+                print(f"Результат: {res}\n")
+                l.log(num_1, num_2, res, choice)
+        else:
+            print("Некорректный ввод")
